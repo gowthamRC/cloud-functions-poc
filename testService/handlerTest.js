2bf75211bf5d9b671dd99b9ef24f017c0cc1da81
Gowtham Ravichandran <gowtham.ravichandran@coda.global>
2018-07-17 12:55:01 +0530

    modifying the cloud function

--- testService/handlerTest.js
+++ testService/handlerTest.js
@@ -1,31 +1,30 @@
 'use strict';
 
-function hello(params) {
-  const name = params.name || 'World';
-  return { payload: `Hello, ${name}!` };
-}
-
+// function hello(params) {
+//   const name = params.name || 'World';
+//   return { payload: `Hello, ${name}!` };
+// }
 
 function hello(params) {
     if(params.firstName != undefined && params.firstName != "" && params.lastName != undefined && params.lastName != "") {
         return {
-            message: 'Hello '+params.firstName+' ' +params.lastName+ '! Deployed function from serverless framework222!',
+            message: 'Hello '+params.firstName+' ' +params.lastName+ '! Deployed function from serverless framework!',
             firstName: params.firstName,
             lastName: params.lastName
         };
     } if(params.firstName != undefined && params.firstName != "") {
         return {
-            message: 'Hello '+params.firstName+ '! Deployed function from serverless framework2222!',
+            message: 'Hello '+params.firstName+ '! Deployed function from serverless framework!',
             firstName: params.firstName
         };
     } if(params.lastName != undefined && params.lastName != "") {
         return {
-            message: 'Hello '+params.lastName+ '! Deployed function from serverless framework222!',
+            message: 'Hello '+params.lastName+ '! Deployed function from serverless framework!',
             lastName: params.lastName
         };
     } else {
         return {
-        message: 'Hello User! Deployed function from serverless framework222!'
+        message: 'Hello User! Deployed function from serverless framework!'
         };
     }
 }
